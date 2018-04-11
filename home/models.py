da355848a6aefb1775141ac131a9c9fb782b6180
Dmitry Duev <dmitry.duev@gmail.com>
2018-04-11 14:19:52 -0700

    work in progress

--- home/models.py
+++ home/models.py
@@ -4,9 +4,31 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
+from stories.models import StoriesPage
+
 
 class HomePage(Page):
     body = RichTextField(blank=True)
+
+    def carousel_stories(self):
+        # Get list of live stories pages
+        stories = StoriesPage.objects.all()
+
+        # Order by most recent date first
+        stories = stories.live().order_by('-date')
+
+        # return only 3 most recent entries
+        return [s for s in stories if 'carousel' in [c.name for c in s.categories.all()]][:3]
+
+    def featured_stories(self):
+        # Get list of live stories pages
+        stories = StoriesPage.objects.all()
+
+        # Order by most recent date first
+        stories = stories.live().order_by('-date')
+
+        # return only 6 most recent entries
+        return [s for s in stories if 'featured' in [c.name for c in s.categories.all()]][:6]
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
